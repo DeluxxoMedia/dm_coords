4199ee2f56732311a225589ef1eeff9bc7c5adc4
Deluxxo Media <115818925+DeluxxoMedia@users.noreply.github.com>
2023-07-06 23:27:25 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'Deluxxo Media'
+
+ui_page 'html/index.html'
+
+
+client_script 'client.lua'
+
+files {
+	'html/index.html',
+	'html/jquery.js',
+	'html/init.js',
+}
